18a0f29ecaba18165906fde385ac4c9384fc2027
Guy Royse <guy@guyroyse.com>
2019-11-06 12:07:29 +0100

    cleaner JS with async await and querySelector

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
 
   let memory = new WebAssembly.Memory({ initial: 1 })
 
   let passiveView = (() => {
 
-    let valueInput = document.getElementById('value')
-    let resultText = document.getElementById('result')
+    let valueInput = document.querySelector('#value')
+    let resultText = document.querySelector('#result')
 
     function getInput() {
       return Number(valueInput.value)
@@ -19,36 +19,22 @@ document.addEventListener('DOMContentLoaded', () => {
 
   })()
 
-  loadWasm('/math.wasm')
-    .then(math => loadWasm('/strings.wasm', { js: { memory }, math }))
-    .then(strings =>
-      Promise.all([
-        loadWasm('model.wasm'),
-        loadWasm('view.wasm', {
-          passiveView,
-          js: { memory },
-          strings
-        })
-      ]))
-    .then(modelAndView =>
-      loadWasm('controller.wasm', {
-        model: modelAndView[0],
-        view: modelAndView[1]
-      }))
-    .then(controller => {
-      let fizzBuzzButton = document.getElementById('fizzbuzz')
-      fizzBuzzButton.addEventListener('click', () => controller.onButtonClicked())
-    })
+  let math = await loadWasm('math.wasm')
+  let strings = await loadWasm('strings.wasm', { js: { memory }, math })
+  let model = await loadWasm('model.wasm')
+  let view = await loadWasm('view.wasm', { js: { memory }, passiveView, strings })
+  let controller = await loadWasm('controller.wasm', { model, view })
+
+  let fizzBuzzButton = document.querySelector('#fizzbuzz')
+  fizzBuzzButton.addEventListener('click', () => controller.onButtonClicked())
 
 })
 
-function loadWasm(path, imports) {
-  return fetch(path)
-    .then(response => response.arrayBuffer())
-    .then(bytes => WebAssembly.instantiate(bytes, imports))
-    .then(results => {
-      return results.instance.exports
-    })
+async function loadWasm(path, imports) {
+  let response = await fetch(path)
+  let bytes = await response.arrayBuffer()
+  let module = await WebAssembly.instantiate(bytes, imports)
+  return module.instance.exports
 }
 
 function decodeString(memory, offset) {
